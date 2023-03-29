b2d618d36ba820fdc8747ab69974886e9c435f49
Matthew Kidd <mattofthekidd@users.noreply.github.com>
2023-03-29 12:48:12 -0600

    Used Chat GPT to convert authorization code to Angular. Setting up basic page layout and login modal

---
+++ client/src/app/constants/spotify-auth.ts
@@ -0,0 +1,9 @@
+export const CLIENT_ID     = '00b49456c4994a4588d27d1c3250bdca';     // Your client id
+export const CLIENT_SECRET = '7d133a7f99044b95bec815c9578b8f27'; // Your secret
+export const REDIRECT_URI  = 'http://localhost:8888/callback';    // Your redirect uri
+export const API_URL       = 'https://accounts.spotify.com/api/token'; //noticed this was used twice so I moved it here.
+export const AUTH_URL      = 'https://accounts.spotify.com/authorize';
+
+// Their are a lot more variables that are considered constants (strings) that can be moved here.
+// For now I want to focus on getting the app working.
+// TODO: move anything with a url string into here.

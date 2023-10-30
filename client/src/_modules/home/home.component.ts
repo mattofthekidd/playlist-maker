a97288aa3bc143260b0bb53b63bdac4403ff4ebf
yashablood <yashablood@yahoo.com>
2023-10-30 04:40:05 -0600

    Added text to the main flex

--- client/src/_modules/home/home.component.ts
+++ client/src/_modules/home/home.component.ts
@@ -5,6 +5,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { SpotifyAuthService } from 'src/_services/spotify-auth.service';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
@@ -15,12 +16,28 @@ import { SpotifyAuthService } from 'src/_services/spotify-auth.service';
     MatDividerModule,
     MatToolbarModule,
     MatIconModule,
+    MatListModule,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   constructor(private spotifyAuthService: SpotifyAuthService) {}
+public fakeList_sideMenu = [
+  {name: "one", description: "first item"},
+  {name: "two", description: "second item"},
+  {name: "three", description: "third item"},
+  {name: "four", description: "fourth item"},
+  
+]
+public fakeList_mainMenu = [
+  {name: "one", description: "first item"},
+  {name: "two", description: "second item"},
+  {name: "three", description: "third item"},
+  {name: "four", description: "fourth item"},
+  
+]
+
   public login(): void {
     this.spotifyAuthService.initiateLogin();
   }

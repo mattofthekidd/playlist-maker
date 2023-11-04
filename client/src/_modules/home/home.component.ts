fe09b69aa81cbb9fd62d50314a8c3b5202e524a4
yashablood <yashablood@yahoo.com>
2023-11-04 12:55:35 -0600

    Changed background color and some text

--- client/src/_modules/home/home.component.ts
+++ client/src/_modules/home/home.component.ts
@@ -5,7 +5,8 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { SpotifyAuthService } from 'src/_services/spotify-auth.service';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-home',
@@ -17,29 +18,41 @@ import {MatListModule} from '@angular/material/list';
     MatToolbarModule,
     MatIconModule,
     MatListModule,
+    MatTooltipModule,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   constructor(private spotifyAuthService: SpotifyAuthService) {}
-public fakeList_sideMenu = [
-  {name: "one", description: "first item"},
-  {name: "two", description: "second item"},
-  {name: "three", description: "third item"},
-  {name: "four", description: "fourth item"},
-  
-]
-public fakeList_mainMenu = [
-  {name: "one", description: "first item"},
-  {name: "two", description: "second item"},
-  {name: "three", description: "third item"},
-  {name: "four", description: "fourth item"},
-  
-]
 
+  public fakeList_sideMenu = [
+    { name: 'one', description: 'first item' },
+    { name: 'two', description: 'second item' },
+    { name: 'three', description: 'third item' },
+    { name: 'four', description: 'fourth item' },
+  ];
+  public fakeList_mainMenu = [
+    { name: 'Song Title', description: 'Artist' },
+    { name: 'Song Title', description: 'Artist' },
+    { name: 'Song Title', description: 'Artist' },
+    { name: 'Song Title', description: 'Artist' },
+  ];
+
+  public loadHome() {
+    console.log('route to home');
+  }
+  public loadLibrary() {
+    console.log('route to library');
+  }
+  public loadProfile() {
+    console.log('route to profile');
+  }
   public login(): void {
     this.spotifyAuthService.initiateLogin();
   }
   public searchMusic(): void {}
+  public account() {
+    console.log('route to account');
+  }
 }
